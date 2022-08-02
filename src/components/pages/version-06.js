b0375d89d58034c90c1f726eb942aa0bccc4dbc3
sakhawat9 <sakhawathossain9769@gmail.com>
2022-08-02 10:16:14 +0600

    update

--- src/components/pages/version-06.js
+++ src/components/pages/version-06.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Aboutv6 from '../aboutv6';
+import Achievement from '../achievement';
 import Booksv1 from '../booksv1';
+import Cta from '../cta';
+import Event from '../event';
+import FunFacts from '../fun-facts';
 import Herov6 from '../herov6';
 
 const Version06 = () => {
@@ -9,6 +13,10 @@ const Version06 = () => {
            <Herov6 />
            <Aboutv6 />
            <Booksv1 /> 
+           <FunFacts />
+           <Achievement />
+           <Cta />
+           <Event />
         </>
     );
 };
