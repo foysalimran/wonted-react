faa5c83a693bcc228e5334233a59ae20423f8836
monirul-fahad <immonirulfahad@gmail.com>
2022-08-03 15:10:09 +0600

    subscribe form

--- src/components/cta.js
+++ src/components/cta.js
@@ -1,34 +1,106 @@
-import React from 'react';
-import data from "../data/cta.json"
+import React from "react";
+import data from "../data/cta.json";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
 
-import {Formik, Form, Field, ErrorMessage, FormikHelpers} from 'formik';
-import * as Yup from 'yup';
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email")
+    .required("You must have to write an email!!"),
+});
+
+const url =
+  "https://themeatelier.us17.list-manage.com/subscribe/post?u=318da6141291eeac976c39d64&amp;id=4297abfa34";
+
+// simplest form (only email)
+const SimpleForm = () => <MailchimpSubscribe url={url} />;
 
 const Cta = () => {
-  const {cta} = data;
-    return (
-        <section className="cta section-padding" id="cta" style={{backgroundImage: `url("${cta.backgroundImage}")`}}>
-        <div className="container">
-          <div className="row justify-content-center cta__inner bg-one">
-            <div className="col-lg-8">
-              <div className="section-title-center mb-0 text-center">
-                <span
-                  data-aos="fade-up"
-                  data-aos-duration="1000"
-                  data-aos-delay="150"
-                  >{cta.subtitle}</span>
-                <h2
-                  className="display-6"
-                  data-aos="fade-up"
-                  data-aos-duration="1000"
-                  data-aos-delay="200"
+  const { cta } = data;
+
+  const submitForm = async (values) => {
+    console.log(values);
+  };
+
+  return (
+    <section
+      className="cta section-padding"
+      id="cta"
+      style={{ backgroundImage: `url("${cta.backgroundImage}")` }}
+    >
+      <div className="container">
+        <div className="row justify-content-center cta__inner bg-one">
+          <div className="col-lg-8">
+            <div className="section-title-center mb-0 text-center">
+              <span
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="150"
+              >
+                {cta.subtitle}
+              </span>
+              <h2
+                className="display-6"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="200"
+              >
+                {cta.title}
+              </h2>
+            </div>
+            <div className="cta-form-box">
+              <MailchimpSubscribe
+                url={url}
+                render={({ subscribe, status, message }) => (
+                  // <div>
+                  //   <SimpleForm
+                  //     onSubmitted={(formData) => subscribe(formData)}
+                  //   />
+                  //   {status === "sending" && (
+                  //     <div style={{ color: "blue" }}>sending...</div>
+                  //   )}
+                  //   {status === "error" && (
+                  //     <div
+                  //       style={{ color: "red" }}
+                  //       dangerouslySetInnerHTML={{ __html: message }}
+                  //     />
+                  //   )}
+                  //   {status === "success" && (
+                  //     <div style={{ color: "green" }}>Subscribed !</div>
+                  //   )}
+                  // </div>
+                  <Formik
+                  initialValues={{ email: "" }}
+                  onSubmit={submitForm}
+                  validationSchema={SignupSchema}
                 >
-                  {cta.title}
-                </h2>
-              </div>
-              <div className="cta-form-box">
-                {/* subscription form start  */}
-                <form action="assets/subscribe/subscribe.php" id="subscribe">
+                  {(formik) => (
+                    <Form>
+                      <Field name="email"></Field>
+                      <button
+                        disabled={!formik.isValid || !formik.dirty}
+                        className="w-100 button button__primary align-items-center"
+                        id="subscribe-button"
+                        type="submit"
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                        data-aos-delay="300"
+                      >
+                        <span>
+                          Subscribe Now <i className="icofont-arrow-right"></i>
+                        </span>
+                      </button>
+                      <ErrorMessage className="error-msg" name="email" />
+                    </Form>
+                  )}
+                </Formik>
+                )}
+              />
+
+              {/* subscription form start  */}
+              {/* <form action="assets/subscribe/subscribe.php" id="subscribe">
                   <div
                     className="mb13"
                     data-aos="fade-up"
@@ -72,9 +144,9 @@ const Cta = () => {
                       <i className="icofont-close"></i> Sorry! Something went wrong!
                     </p>
                   </div>
-                </form>
-                 {/* Mailchimp subscription form  */}
-                {/* <form
+                </form> */}
+              {/* Mailchimp subscription form  */}
+              {/* <form
                   action="#"
                   id="subscribe-mailchimp"
                   data-wow-duration="1.5s"
@@ -106,13 +178,13 @@ const Cta = () => {
                     * eBook includes iBooks, PDF &amp; ePub versions
                   </p>
                 </form>  */}
-                {/* <!-- subscription form end --> */}
-              </div>
+              {/* <!-- subscription form end --> */}
             </div>
           </div>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Cta;
